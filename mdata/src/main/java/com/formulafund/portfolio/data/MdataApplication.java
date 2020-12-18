e840b39f27d685ca8d2b0012cdc4c3a5c52854c5
olsongrant <grant.olson@gmail.com>
2020-12-18 11:16:43 -0600

    base multi-module commit

---
+++ mdata/src/main/java/com/formulafund/portfolio/data/MdataApplication.java
@@ -0,0 +1,13 @@
+package com.formulafund.portfolio.data;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MdataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MdataApplication.class, args);
+	}
+
+}
---
+++ mdata/src/test/java/com/formulafund/portfolio/data/MdataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.formulafund.portfolio.data;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MdataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
