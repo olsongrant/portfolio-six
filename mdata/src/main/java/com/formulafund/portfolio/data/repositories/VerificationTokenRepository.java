9db9ef1111c4516ebaad074f4ef5349099d6b98f
olsongrant <grant.olson@gmail.com>
2021-01-18 10:40:28 -0600

    more passwordreset and verification token stuff

--- mdata/src/main/java/com/formulafund/portfolio/data/repositories/VerificationTokenRepository.java
+++ mdata/src/main/java/com/formulafund/portfolio/data/repositories/VerificationTokenRepository.java
@@ -1,15 +1,24 @@
 package com.formulafund.portfolio.data.repositories;
 
 import java.util.Date;
+import java.util.stream.Stream;
 
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
-import com.formulafund.portfolio.data.model.IssuingCompany;
+import com.formulafund.portfolio.data.model.ApplicationUser;
 import com.formulafund.portfolio.data.model.VerificationToken;
 
 public interface VerificationTokenRepository extends CrudRepository<VerificationToken, Long> {
+	
+    VerificationToken findByToken(String token);
+
+    VerificationToken findByUser(ApplicationUser user);
+
+    Stream<VerificationToken> findAllByExpiryDateLessThan(Date now);
+
+    void deleteByExpiryDateLessThan(Date now);
 
     @Modifying
     @Query("delete from VerificationToken t where t.expiryDate <= ?1")
--- mdata/src/main/java/com/formulafund/portfolio/data/services/PasswordResetTokenService.java
+++ mdata/src/main/java/com/formulafund/portfolio/data/services/PasswordResetTokenService.java
@@ -1,7 +1,19 @@
 package com.formulafund.portfolio.data.services;
 
+import java.util.Date;
+import java.util.stream.Stream;
+
+import com.formulafund.portfolio.data.model.ApplicationUser;
 import com.formulafund.portfolio.data.model.PasswordResetToken;
 
 public interface PasswordResetTokenService extends CrudService<PasswordResetToken> {
+    PasswordResetToken findByToken(String token);
 
+    PasswordResetToken findByUser(ApplicationUser user);
+
+    Stream<PasswordResetToken> findAllByExpiryDateLessThan(Date now);
+
+    void deleteByExpiryDateLessThan(Date now);
+
+    void deleteAllExpiredSince(Date now);
 }
